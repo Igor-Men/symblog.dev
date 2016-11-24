c4c93ddf249e6660c488919bfdc0b7ccc38159f6
Igor-Men <dp030988kie@gmail.com>
2016-11-25 01:24:29 +0200

    finish second lesson

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -6,12 +6,17 @@ use Blogger\BlogBundle\Entity\Enquiry;
 use Blogger\BlogBundle\Form\EnquiryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\MinLength;
+use Symfony\Component\Validator\Constraints\MaxLength;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
 class PageController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BloggerBlogBundle:Page:index.html.twig');
-    }
 
     public function aboutAction()
     {
@@ -20,18 +25,34 @@ class PageController extends Controller
 
     public function contactAction()
     {
+        $session = new Session();
+        $session->start();
+
         $enquiry = new Enquiry();
         $form = $this->createForm(new EnquiryType(), $enquiry);
 
         $request = $this->getRequest();
         if ($request->getMethod() == 'POST') {
-            $form->bindRequest($request);
+            $form->handleRequest($request);
 
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
 
-                // Redirect - This is important to prevent users re-posting
-                // the form if they refresh the page
+                $message = \Swift_Message::newInstance()
+                        ->setSubject('Contact enquiry from symblog')
+                        ->setFrom('enquiries@symblog.co.uk')
+                        ->setTo('gwk02099@noicd.com')
+                        ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                    // need rework -------------->>>>>>>>>>>
+                // {% if session.hasFlash('blogger-notice') %}
+                //     <div class="blogger-notice">
+                //         {{ session.flash('blogger-notice') }}
+                //     </div>
+                // {% endif %}
+
+                //$session->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
                 return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
             }
         }
@@ -39,5 +60,24 @@ class PageController extends Controller
         return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email(array(
+            'message' => 'symblog does not like invalid emails. Give me a real one!'
+        )));
+
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('subject', new MaxLength(50));
+
+        $metadata->addPropertyConstraint('body', new MinLength(50));
     }
 }
